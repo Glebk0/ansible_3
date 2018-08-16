58ea90614b6156c3d7c0a422a8c1cd472cabf695
Glebk0 <glebko123@gmail.com>
2018-08-16 22:16:28 +0300

    16.08 work

---
+++ filter_plugins/filter.py
@@ -0,0 +1,16 @@
+def sel(arg, name):
+    import re
+
+    result = ""
+    for item in arg:
+        if item['name'] == name:
+             result = item['id']
+
+    return result
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'sel': sel
+        }
